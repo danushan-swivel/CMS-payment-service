cbb6bf48c79c5ebb38586e37bd66ef816a131dca
DanushanKanagasingam <danushan@swivelgroup.com.au>
2023-02-28 12:19:50 +0530

    Initial commit

---
+++ src/main/java/com/cms/payment/CmsPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cms.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsPaymentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cms/payment/CmsPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cms.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
